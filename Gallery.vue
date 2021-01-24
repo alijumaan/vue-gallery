c528c8fe42e0d5464e17c93c9c53965b3998aaae
alila3883 <alila3883@gmail.com>
2021-01-24 23:35:51 +0300

    first commit

---
+++ Gallery.vue
@@ -0,0 +1,24 @@
+<template>
+  {{test}}
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log('mounted')
+  },
+  created() {
+    console.log('created')
+  },
+  setup(){
+    console.log('setup')
+    return {
+      test: 'Ali'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
