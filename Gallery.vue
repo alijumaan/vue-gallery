93bd2d2a85f1eb407dece66c782aba76e98616b8
alila3883 <alila3883@gmail.com>
2021-01-25 11:15:14 +0300

    learning ref & reative method

--- Gallery.vue
+++ Gallery.vue
@@ -1,8 +1,14 @@
 <template>
-  {{test}}
+  <div>
+    <h2 @click="update">{{ text }}</h2>
+    <h2 @click="updateCount">{{ count }}</h2>
+    <h3 @click="updateUser('name', 'Saad')">{{ user.name }}</h3>
+    <h5 @click="updateUser('email', 'saad@gamil.com')">{{ user.email }}</h5>
+  </div>
 </template>
 
 <script>
+import {ref, reactive} from 'vue';
 export default {
   mounted() {
     console.log('mounted')
@@ -11,9 +17,23 @@ export default {
     console.log('created')
   },
   setup(){
-    console.log('setup')
+    const text = ref('Hello')
+    const count = ref(0)
+    const user = reactive({
+      name: 'Ali',
+      email: 'alila3883@gmail.com'
+    })
+    const update = () => {
+      text.value = 'New Hello'
+    }
+    const updateCount = () => {
+      count.value++;
+    }
+    const updateUser = (key, value) => {
+      user[key] = value
+    }
     return {
-      test: 'Ali'
+      text, update, count, updateCount, user, updateUser
     }
   }
 }
